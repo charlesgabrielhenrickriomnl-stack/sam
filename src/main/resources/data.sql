2629c564a6489e4aeb2b694ee92384d31cc10e5f
Ycharlesgabrielhenrickriomnl-stack <charlesgabrielhenrick.mnl@eac.edu.ph>
2025-12-15 15:49:48 +0800

    data.sql fix 1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,7 +10,7 @@ VALUES
 ('CS202', 'Data Structures', 2, 1, 3, '2.00', 'MW 1:30 PM - 3:00 PM'),
 ('MATE1', 'College Algebra', 3, 0, 3, '3.00', 'TTh 9:30 AM - 11:00 AM'),
 ('FIL12', 'Filipino Subject', 3, 0, 3, 'INC', 'F 3:30 PM - 5:30 PM'),
-('PE301', 'Physical Fitness', 2, 0, 2, 'PASSED', 'Sat 7:30 AM - 9:30 AM');
+('PE301', 'Physical Fitness', 2, 0, 2, 'PASSED', 'Sat 7:30 AM - 10:30 AM');
 
 -- Insert default department user with encoded password for 'departmentpass'
 INSERT INTO users (username, email, password, enabled, first_name, last_name) VALUES 
