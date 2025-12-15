464ba6f95585a57eae381efcc808c94e7a246cc5
Ycharlesgabrielhenrickriomnl-stack <charlesgabrielhenrick.mnl@eac.edu.ph>
2025-12-15 15:38:53 +0800

    automatic execution of .sql files

--- src/main/java/auth/proj/sam/repository/SubjectRepository.java
+++ src/main/java/auth/proj/sam/repository/SubjectRepository.java
@@ -4,6 +4,9 @@ import auth.proj.sam.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    Optional<Subject> findByCode(String code);
 }
